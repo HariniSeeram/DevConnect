cd35695cd96be157fd8d8be4a6d970b57f921bbc
HariniSeeram <seeramharini1@gmail.com>
2026-05-15 13:37:41 +0530

    Updated backend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://your-railway-url.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
